bd51c827ff29a8248be4c397ab6c284a8dcdb9a3
Daniel <dblanzacogl@gmail.com>
2018-10-29 10:45:28 +0100

    RESTServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 require('./config/config');
 
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+
 const bodyParser = require('body-parser');
+
+const app = express();
+
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -10,33 +15,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({ persona: body });
-    }
+app.use(require('./routes-controllers/usuario'));
 
 
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({ id });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
+mongoose.connect(process.env.URLDB, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE!!!');
 });
 
 app.listen(process.env.PORT, () => {
